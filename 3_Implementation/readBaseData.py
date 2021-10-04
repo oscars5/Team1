6f4e508b56e2e0527ca79bc25e329001c1011ad1
oscars5 <kumar.ashish2408@gmail.com>
2021-10-04 20:49:54 +0530

    edited

--- 3_Implementation/readBaseData.py
+++ 3_Implementation/readBaseData.py
@@ -3,6 +3,8 @@ import openpyxl as xl
 from openpyxl.chart import BarChart, PieChart, Reference
 from openpyxl.styles.borders import Border, Side
 from openpyxl.styles import Alignment, Color, PatternFill, Font
+import pandas as pd
+import numpy as np
 from copy import copy
 
 # import plotly
@@ -48,17 +50,16 @@ add_border = Border(left=Side(style='thin'),  # setting border
 x = 0
 for l in range(6, mc + 1):
     b = workSheet1.cell(row=1, column=l)
-    workSheetFinal.cell(row=1, column=l + 2 * x + 2).value = b.value
+    workSheetFinal.cell(row=1, column=l + x + 1).value = b.value
     x = x + 1
 
 y = 1
 for k in range(6, mc + 1):
     workSheetFinal.cell(row=2, column=k + y, value='Score')
     workSheetFinal.cell(row=2, column=k + y + 1, value='Grades')
-    workSheetFinal.cell(row=2, column=k + y + 2, value='Credits')
-    y = y + 2
+    # workSheetFinal.cell(row=2, column=k + y + 2, value='Credits')
+    y = y + 1
 workSheetFinal.cell(row=2, column=k + y + 1, value='Average Score')
-workSheetFinal.cell(row=2, column=6, value='CGPA')
 
 # _______________________________RAW SCORE ENTRY,GRADES AND AVERAGE CALCULATION_______________________________________
 for m in range(3, mr + 2):
@@ -68,10 +69,14 @@ for m in range(3, mr + 2):
         c = workSheet1.cell(row=m - 1, column=n)
         eTotal = eTotal + c.value
         workSheetFinal.cell(row=m, column=n + z).value = c.value
+        workSheetFinal.cell(row=m, column=n + z).alignment = Alignment(horizontal='center', vertical='center')
+        workSheetFinal.cell(row=m, column=n + z).fill = PatternFill(patternType='solid', fgColor='87D700')
         if 90 <= c.value <= 100:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'A+'
+            workSheetFinal.cell(row=m, column=n + z).fill = PatternFill(patternType='solid', fgColor='EE3673')
         elif 85 <= c.value < 90:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'A'
+            workSheetFinal.cell(row=m, column=n + z).fill = PatternFill(patternType='solid', fgColor='4152BD')
         elif 80 <= c.value < 85:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'A-'
         elif 75 <= c.value < 80:
@@ -82,14 +87,19 @@ for m in range(3, mr + 2):
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'B-'
         elif 60 <= c.value < 65:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'C+'
+            workSheetFinal.cell(row=m, column=n + z).fill = PatternFill(patternType='solid', fgColor='A0A0A0')
         elif 55 <= c.value < 60:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'C'
+            workSheetFinal.cell(row=m, column=n + z).fill = PatternFill(patternType='solid', fgColor='C0C0C0')
         elif 50 <= c.value < 55:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'C-'
         else:
             workSheetFinal.cell(row=m, column=n + z + 1).value = 'F'
-        z = z + 2
+        workSheetFinal.cell(row=m, column=n + z + 1).alignment = Alignment(horizontal='center', vertical='center')
+        workSheetFinal.cell(row=m, column=n + z+1).fill = PatternFill(patternType='solid', fgColor='87FFAF')
+        z = z + 1
     workSheetFinal.cell(row=m, column=k + y + 1, value=eTotal / (mc - 5))
+    workSheetFinal.cell(row=m, column=k + y + 1).fill = PatternFill(patternType='solid', fgColor='FF00FF')
 
 print(c.value)
 
@@ -101,35 +111,23 @@ for i in range(1, mr + 1):
         # writing the read value to destination excel file
         workSheetFinal.cell(row=i + 1, column=j).value = c.value
 
-# ________________________________________________Credits Copying____________________________________________
-
-for m1 in range(3, mr + 2):
-    z1 = 1
-    x1 = 0
-
-    for n1 in range(6, mc + 1):
-        c = workSheet2.cell(row=2, column=n1 + x1 - 3)
-        x1 = x1 + 1
-
-        workSheetFinal.cell(row=m1, column=n1 + z1 + 2).value = c.value
-        z1 = z1 + 2
-
-print(c.value)
-
 # ____________________________________# TODO MIN, MAX, SD, TOP-10, BOTTOM-10___________________________________________
 
 
 workSheetFinal.cell(row=mr + 3, column=5, value='AVERAGE')
+workSheetFinal.cell(row=mr + 3, column=5).fill = PatternFill(patternType='solid', fgColor='FF00FF')
 workSheetFinal.cell(row=mr + 4, column=5, value='MAX')
+workSheetFinal.cell(row=mr + 4, column=5).fill = PatternFill(patternType='solid', fgColor='FF5FAF')
 workSheetFinal.cell(row=mr + 5, column=5, value='MIN')
-workSheetFinal.cell(row=mr + 6, column=5, value='SD')
-workSheetFinal.cell(row=mr + 7, column=5, value='KURT')
-
+workSheetFinal.cell(row=mr + 5, column=5).fill = PatternFill(patternType='solid', fgColor='FF0000')
+workSheetFinal.cell(row=mr + 6, column=5, value='')
+workSheetFinal.cell(row=mr + 7, column=5, value='')
+#
 mrf = workSheetFinal.max_row
 mcf = workSheetFinal.max_column
 print(mrf, mcf)
 
-for col in range(7, mcf, 3):
+for col in range(7, mcf, 2):
     max = 0
     min = 100
     mTotal = 0
@@ -145,8 +143,14 @@ for col in range(7, mcf, 3):
     print(max)
     print(min)
     workSheetFinal.cell(row=row + 2, column=col, value=mTotal / (mr - 1))
+    workSheetFinal.cell(row=row + 2, column=col).alignment = Alignment(horizontal='center', vertical='center')
+    workSheetFinal.cell(row=row + 2, column=col).fill = PatternFill(patternType='solid', fgColor='FF00FF')
     workSheetFinal.cell(row=row + 3, column=col, value=max)
+    workSheetFinal.cell(row=row + 3, column=col).alignment = Alignment(horizontal='center', vertical='center')
+    workSheetFinal.cell(row=row + 3, column=col).fill = PatternFill(patternType='solid', fgColor='FF5FAF')
     workSheetFinal.cell(row=row + 4, column=col, value=min)
+    workSheetFinal.cell(row=row + 4, column=col).alignment = Alignment(horizontal='center', vertical='center')
+    workSheetFinal.cell(row=row + 4, column=col).fill = PatternFill(patternType='solid', fgColor='FF0000')
 print(mTotal)
 
 
